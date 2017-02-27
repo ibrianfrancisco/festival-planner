29674558acdf9883f3e25cc6a419f6d26e646979
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-26 21:34:55 -0800

    fixed merge conflict

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,7 +3,8 @@
 
   angular.module('app', ['ui.router', 'ngAnimate', 'ngResource'])
     .config(configRoutes)
-    .run(runBlock);
+    .run(runBlock)
+    .run(loginBlock);
 
     runBlock.$inject = ['$rootScope', '$state', 'UserService'];
 
@@ -12,6 +13,18 @@
         if(toState.loginRequired && !UserService.isLoggedIn()) {
           evt.preventDefault();
           $state.go('login');
+        }
+      });
+    }
+
+    loginBlock.$inject = ['$rootScope', '$state', 'UserService'];
+
+
+    function loginBlock($rootScope, $state, UserService) {
+      $rootScope.$on('$stateChangeStart', function(evt, toState) {
+        if(toState.loggedIn && UserService.isLoggedIn()) {
+          evt.preventDefault();
+          $state.go('homepage');
         }
       });
     }
@@ -32,13 +45,15 @@
       .state('login', {
         url: '/login',
         templateUrl: 'templates/users/login.html',
-        controller: 'UserController as userCtrl'
+        controller: 'UserController as userCtrl',
+        loggedIn: true
       })
 
       .state('signup', {
         url: '/signup',
         templateUrl: 'templates/users/signup.html',
-        controller: 'UserController as userCtrl'
+        controller: 'UserController as userCtrl',
+        loggedIn: true
       })
 
     $urlRouterProvider.otherwise('/home');
