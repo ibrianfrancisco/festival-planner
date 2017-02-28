f06a17c4815f1d2378d88a4ab4647e7bb5b01f4b
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-27 23:45:14 -0800

    can now add festival to mlab

---
+++ controllers/festivals.js
@@ -0,0 +1,32 @@
+var Festival = require('../models/festival');
+// not necessary?
+var ObjectId = require('mongoose').Schema.Types.ObjectId;
+
+module.exports = {
+  getAllFestivals,
+  createFestival,
+  deleteFestival
+}
+
+function getAllFestivals(req, res, next) {
+  console.log('Get All Triggered');
+  Festival.find({user: req.user._id}).exec().then(festivals => {
+    res.json(festivals);
+    // console.log(festivals)
+  }).catch(err => res.status(500).json(err));
+}
+
+function createFestival(req, res, next) {
+  console.log('create triggered');
+  req.body.user = req.user._id;
+  Festival.create(req.body).then(newFestival => {
+    res.status(201).json(newFestival);
+  }).catch(err => res.status(400).json(err));
+}
+
+function deleteFestival(req, res, next) {
+  Festival.findByIdAndRemove(req.params.id)
+  .then(deletedFest => {
+    res.json(deletedFest);
+  }).catch(err => res.status(400).json(err));
+}
