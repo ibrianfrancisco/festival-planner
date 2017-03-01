6e1d494784d568e3ec216ba4ee9f5ffdeb07713b
Brian Francisco <ibrianfrancisco@gmail.com>
2017-03-01 10:33:11 -0800

    deleted datetime

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 (function() {
 'use strict';
 
-  angular.module('app', ['datetime','ui.router', 'ngAnimate', 'ngResource'])
+  angular.module('app', ['ui.router', 'ngAnimate', 'ngResource'])
     .config(configRoutes)
     .run(runBlock)
     .run(loginBlock);
