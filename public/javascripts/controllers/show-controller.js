6d6f1c5839b09619518564739782434a910d64ce
Brian Francisco <ibrianfrancisco@gmail.com>
2017-03-03 03:24:13 -0800

    styled festival page

--- public/javascripts/controllers/show-controller.js
+++ public/javascripts/controllers/show-controller.js
@@ -9,37 +9,15 @@
 
   function ShowController($stateParams, Festival, datetime) {
     var vm = this;
-    var parser = datetime("hh:mm a");
 
-    // vm.test = parser.setDate(fes);
-
-
-    // Margin-left
-    // Every 5 minutes is 0.69444444%
-
-    // width is the
-
-
-    // temporary template used to display timeline
     vm.numbers = ["12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00"];
 
-
-    // $stateParams is the id that gets put into the url. then these get and delete things happen from this page
     vm.festival = Festival.get({id: $stateParams.id});
 
     vm.addStage = function() {
-      //assume vm.festival already exists
-      // vm.festival = {_id: $stateParams.id};
-      // festId goes to controllers/festivals and vm.festival._id is the festival id that's provded already
-      // MARKER 2 stageName goes to controllers/festivals // vm.newStageName one comes from the template
       Festival.addStage({festId: vm.festival._id, stageName: vm.stageName}, function(festival) {
-        // Now this is how once the user clicks 'save stage', it will automatcally update,
-        // because once it goes to the database, it comes right back into this argument
-        // and from here , goes to browser template and has the power to call ? idk if get route is needed
         vm.festival = festival;
         $('#stage-input').val('');
-        console.log(festival);
-        // console.log('stage made');
       });
     }
 
@@ -51,10 +29,7 @@
         actEndTime: vm.actEndTime
       }, function(festival) {
         vm.festival = festival;
-        console.log(festival);
         $('#artist-name').val('');
-        // $('#act-start-time').val('');
-        // $('#act-end-time').val('');
       });
     }
 
@@ -64,7 +39,6 @@
       var dt = new Date(dateStr);
       var baseDate = new Date(dateStr).setHours(12, 0, 0, 0);
       var hrs = (dt - baseDate) / (1000 * 60 * 60);
-      console.log('hrs', hrs)
       return (hrs * offsetPerHour) + 'px';
     }
 
@@ -77,12 +51,9 @@
 
     vm.formatTime = function(dateStr) {
       var dt = new Date(dateStr);
-      return dt.getHours() + ':' + dt.getMinutes();
+      return (dt.getHours() - 12) + ':' + ('0' + dt.getMinutes()).slice(-2);
     }
 
-    $("#artist-box").on( "click", function( event ) {
-      $( event.delegateTarget ).css( "background-color", "red" );
-    });
   }
 
 })();
