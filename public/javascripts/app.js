d91db1567d6b8e7ee13623c28fc6e8081b20329f
Brian Francisco <ibrianfrancisco@gmail.com>
2017-03-31 22:14:16 -0700

    styled festival page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 (function() {
 'use strict';
 
-  angular.module('app', ['ui.router', 'ngAnimate', 'ngResource', 'datetime'])
+  angular.module('app', ['ui.router', 'ngAnimate', 'ngResource'])
     .config(configRoutes)
     .run(runBlock)
     .run(loginBlock);
--- public/javascripts/controllers/show-controller.js
+++ public/javascripts/controllers/show-controller.js
@@ -5,16 +5,19 @@
   angular.module('app')
   .controller('ShowController', ShowController);
 
-  ShowController.$inject = ['$stateParams', 'Festival', 'datetime'];
+  ShowController.$inject = ['$stateParams', 'Festival'];
 
-  function ShowController($stateParams, Festival, datetime) {
+  function ShowController($stateParams, Festival) {
     var vm = this;
 
-    vm.timeline = ["12:00", "1:00", "2:00",
+    vm.timeline = ["0:00", "1:00", "2:00",
                    "3:00", "4:00", "5:00",
                    "6:00", "7:00", "8:00",
                    "9:00", "10:00", "11:00",
-                   "12:00"];
+                   "12:00","13:00", "14:00",
+                   "15:00", "16:00", "17:00",
+                   "18:00", "19:00", "20:00",
+                   "21:00", "22:00", "23:00"];
 
     vm.festival = Festival.get({id: $stateParams.id});
 
@@ -40,7 +43,7 @@
     vm.leftOffset = function (dateStr) {
       var offsetPerHour = 1072/12;
       var dt = new Date(dateStr);
-      var baseDate = new Date(dateStr).setHours(12, 0, 0, 0);
+      var baseDate = new Date(dateStr).setHours(0, 0, 0, 0);
       var hrs = (dt - baseDate) / (1000 * 60 * 60);
       return (hrs * offsetPerHour) + 'px';
     }
@@ -54,7 +57,7 @@
 
     vm.formatTime = function(dateStr) {
       var dt = new Date(dateStr);
-      return (dt.getHours() - 12) + ':' + ('0' + dt.getMinutes()).slice(-2);
+      return (dt.getHours()) + ':' + ('0' + dt.getMinutes()).slice(-2);
     }
 
   }
